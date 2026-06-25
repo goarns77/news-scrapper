6fc26ab61a878fd8c4b8e397e7156e389d363d74
goarns77 <goarns771@gmail.com>
2026-06-25 15:57:59 +0900

    feat:fixed NEWS_CATEGORY

--- src/oop/search/presentation/GitHubNewsApp.java
+++ src/oop/search/presentation/GitHubNewsApp.java
@@ -4,8 +4,6 @@ import oop.search.application.NewsService;
 import oop.search.infrastructure.GitHubNewsPublisher;
 import oop.search.infrastructure.NaverNewsProvider;
 
-import java.util.Scanner;
-
 public class GitHubNewsApp {
     private final NewsService newsService;
 
@@ -14,28 +12,18 @@ public class GitHubNewsApp {
     }
 
     public void run() {
-        Scanner sc = new Scanner(System.in);
-        while (true) { // 예외처리는 생략
-            System.out.println("[검색할 키워드를 입력해주세요 (q 입력 시 종료)]");
-            String keyword = sc.nextLine().trim(); // ?ㅽ럹?댁뒪 臾몄젣 ?쒓굅
-            if (keyword.equals("q")) {
-                System.out.println("[검색을 종료합니다]");
-                break;
-            }
-            System.out.println("[몇 건 검색하시겠습니까? (양의 정수)]");
-            int limit = sc.nextInt();
-            sc.nextLine(); // 버퍼 비우기를 안하면 이후 다음 nextLine 호출 시 엔터키 입력으로 인한 빈 문자열 입력
-            newsService.search(keyword, limit);
-            System.out.println("[검색이 완료 되었습니다]");
-        }
+        String keyword = System.getenv("NEWS_QUERY");
+        String limit = System.getenv("NEWS_DISPLAY");
+        newsService.search(keyword, Integer.parseInt(limit));
     }
 
     public static void main(String[] args) {
         NewsService newsService = new NewsService(
                 new GitHubNewsPublisher(),
-                new NaverNewsProvider()
+                new NaverNewsProvider() // 그대로 두고
+//                new ConsoleNewsPublisher()
         );
-        ConsoleNewsApp app = new ConsoleNewsApp(newsService);
+        GitHubNewsApp app = new GitHubNewsApp(newsService);
         app.run();
     }
 }
